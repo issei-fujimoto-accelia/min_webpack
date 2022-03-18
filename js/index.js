727dd0554c92ee2dc2ed44e64dd0237b51fe8399
i.fujimoto <i.fujimoto@accelia.net>
2022-03-18 18:12:46 +0900

    add notification

--- js/index.js
+++ js/index.js
@@ -1,5 +1,80 @@
 import {hello, set_title} from './modules/module1.js'
 
-hello()
+const registerSW = () => {
+    if ('serviceWorker' in navigator) {
+	window.addEventListener('load', () => {	    
+	    navigator.serviceWorker.register('/js/sw.js')
+		.then((r) => {
+		    console.log("success", r)
+		})
+		.catch((e) => {
+		    console.log("error", e)
+		})	    
+	});
+    } else {
+	console.log("not support..")
+    }
+}
 
-window.onload = () => {set_title()}
+registerSW()
+
+
+const initSW = () => {
+    navigator.serviceWorker?.ready
+	.then((r) => {
+	    console.log("ready", r)
+	    if(requestPermission()) {
+		popup()
+	    }
+	})
+}
+
+
+
+const setOnClick = () => {
+    const btn = document.getElementById("button")
+    console.log(btn);
+    btn.addEventListener("click", () => {
+	const n = new Notification(
+	    'タイトル',
+	    {	    
+		body: '本文です',
+		tag: 'tag',
+		data: {
+		    xxx: 'データ'
+		}
+	    }	    
+	);
+    })   
+}
+
+
+
+
+const requestPermission = () => {
+    return Notification.requestPermission()
+      .then((permission) => {
+        if (permission == 'granted') {
+            return true
+        } else if (permission == 'denied') {
+	    return false
+        } else if (permission == 'default') {
+            return false
+        }
+      });
+}
+
+
+const main = () => {
+    initSW()
+    hello()
+    set_title()
+    requestPermission()
+    setOnClick()
+}
+
+window.onload = () => {
+    main()
+}
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,32 @@
 var debug = process.env.NODE_ENV !== "production";
 var path = require('path')
 var webpack = require('webpack');
+const CopyPlugin = require("copy-webpack-plugin");
+
 
 module.exports = {
     context: __dirname,
-    entry: "./js/index.js",
+    entry: {
+	app: "./js/index.js"
+    },
     devServer: {
 	open: true,
         openPage: "index.html",
-        contentBase: path.join(__dirname, 'public'),
+        contentBase: path.join(__dirname, 'dist'),
 	watchContentBase: true,
         port: 8080,
-        host: `localhost`,
+        host: `0.0.0.0`,
     },
     output: {
-	path: __dirname + "/dist",
+	filename: '[name].js',
 	publicPath: '/js/',
-	filename: "app.js"
-    }  
+	path: __dirname + '/dist'
+    },
+    plugins: [
+	new CopyPlugin({
+	    patterns: [
+		"./js/sw.js",		
+	    ],
+	}),
+    ]
 };
